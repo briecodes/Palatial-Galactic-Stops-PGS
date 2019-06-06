e8d7c4a53009c4dac3c10e2265e142161c4de705
briecodes <hello@brie.work>
2019-06-06 15:00:13 -0400

    Added view start and layout pages.

--- PalatialGalacticStops/Controllers/HomeController.cs
+++ PalatialGalacticStops/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace PalatialGalacticStops.Controllers
       var places = _placeRepository.GetAllPlaces().OrderBy(p => p.Name);
       var homeViewModel = new HomeViewModel()
       {
-        Title = $"Number of places: {places.Count()}",
+        Title = "Welcome to PGS!",
         Places = places.ToList(),
       };
 
