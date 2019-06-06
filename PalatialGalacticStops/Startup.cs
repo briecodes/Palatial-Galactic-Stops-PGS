e0135c6baf45bbb70c8a94ddf1ff5594b319ea9a
briecodes <hello@brie.work>
2019-06-06 14:20:58 -0400

    Fixed isse within program.cs. Added migrations and created, seeded DB.

--- PalatialGalacticStops/Startup.cs
+++ PalatialGalacticStops/Startup.cs
@@ -42,7 +42,7 @@ namespace PalatialGalacticStops
         {
           routes.MapRoute(
             name: "default",
-            template: "{controller=Home/{action=Index}/{Id?}"
+            template: "{controller=Home}/{action=Index}/{id?}"
             );
         });
       }
---
+++ PalatialGalacticStops/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PalatialGalacticStops.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PalatialGalacticStops.ViewModels
+{
+  public class HomeViewModel
+  {
+    public string Title { get; set; }
+  }
+}
